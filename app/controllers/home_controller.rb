3ae204e9c8edaaa665d890ad5e0bb2dc38ff6a16
Marcin Mantke <mentosso@gmail.com>
2015-11-28 19:18:18 +0100

    Refactor

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -22,52 +22,9 @@ class HomeController < ApplicationController
   end
 
   def temp_chart
-    t_temp = Time.zone.now
-    @t_now = Time.new(t_temp.year, t_temp.month, t_temp.day,
-                      t_temp.hour, t_temp.min, 0, Time.zone.now.strftime('%:z'))
-    # temperature_data = TemperatureData.includes(:sensor).where(created_at: (t_now - 1.hour..t_now))
-    data = []
-    time = create_labels(@t_now)
-    Sensor.all.each do |sensor|
-      temperature_data = TemperatureData.includes(:sensor).where(sensor_id: sensor.id,
-                                                                 created_at: (@t_now - 1.hour..@t_now))
-      temp_data = []
-      @arr_index = 0
-      (0..59).reverse_each do |amount|
-        temperature = get_array_from_minute(amount, temperature_data,
-                                            sensor, @t_now)
-        # time = (@t_now - amount.minute..@t_now - amount.minute + 1.minute)
-        # temperature = TemperatureData.where(sensor_id: sensor.id, created_at: time)
-        if temperature.present?
-          temp_data << (temperature.sum(&:temp) / temperature.length.to_f).round(2)
-        else
-          temp_data << []
-        end
-      end
-      data << temp_data
-    end
+    service = ChartDataService.new(Time.zone.now, 'temperature')
+    time = service.create_labels
+    data = service.retrive_temperature_data
     render json: [data, time, Sensor.all.pluck(:number)]
-  end
-
-  def create_labels(time)
-    arr = []
-    (0..59).reverse_each do |amount|
-      arr << (time - amount.minute).strftime('%R')
-    end
-    arr
-  end
-
-  def get_array_from_minute(amount, arr, sensor, time)
-    data = []
-    arr[@arr_index..-1].each_with_index do |val, index|
-      if val.created_at > time - amount.minute
-        @arr_index = index
-        break
-      end
-      data << val if val.sensor_id == sensor.id &&
-                     val.created_at <= time - amount.minute &&
-                     val.created_at >= time - amount.minute - 1.minute
-    end
-    data
   end
 end
